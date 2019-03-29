ab2cc18caf5c6cc70583a60808b2675858d70e55
Paulo Monteiro <paulo.from.portugal@gmail.com>
2019-03-29 11:53:55 +0000

    CEDS-1055 Create exports-movements service

---
+++ app/uk/gov/hmrc/exports/models/AuthorisedRequest.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.exports.models
+
+import play.api.mvc.{Request, WrappedRequest}
+
+trait HasLocalReferenceNumber {
+  val localReferenceNumber: LocalReferenceNumber
+}
+
+trait HasEori {
+  val eori: Eori
+}
+
+trait HasConversationId {
+  val conversationId: ConversationId
+}
+
+trait HasAuthToken {
+  val authToken: AuthToken
+}
+
+trait HasMrn {
+  val mrn: Mrn
+}
+
+case class LocalReferenceNumber(value: String) extends AnyVal
+case class Ducr(value: String) extends AnyVal
+case class Mrn(value: String) extends AnyVal
+case class Eori(value: String) extends AnyVal
+case class ConversationId(value: String) extends AnyVal
+case class AuthToken(value: String) extends AnyVal
+
+case class AuthorizedSubmissionRequest[A](eori: Eori, request: Request[A])
+    extends WrappedRequest[A](request) with HasEori
+
+case class ValidatedHeadersSubmissionRequest(localReferenceNumber: LocalReferenceNumber, ducr: Option[String])
+    extends HasLocalReferenceNumber
+
+case class ValidatedHeadersCancellationRequest(mrn: Mrn) extends HasMrn
+
+case class MovementNotificationApiRequest(authToken: AuthToken, conversationId: ConversationId, eori: Eori)
+    extends HasEori with HasConversationId with HasAuthToken
+
+case class SubmissionNotificationApiRequest(authToken: AuthToken, conversationId: ConversationId)
+    extends HasConversationId with HasAuthToken
---
+++ build.sbt
@@ -0,0 +1,42 @@
+import sbt._
+import uk.gov.hmrc.DefaultBuildSettings._
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin.publishingSettings
+import uk.gov.hmrc.{SbtArtifactory, SbtAutoBuildPlugin}
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+val appName = "customs-declare-exports-movements"
+
+lazy val microservice = Project(appName, file("."))
+  .enablePlugins(PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin, SbtArtifactory)
+  .settings(
+    libraryDependencies ++= AppDependencies.compile ++ AppDependencies.test(),
+    evictionWarningOptions in update := EvictionWarningOptions.default.withWarnScalaVersionEviction(false),
+    majorVersion := 0
+  )
+  .settings(publishingSettings: _*)
+  .configs(IntegrationTest)
+  .settings(inConfig(IntegrationTest)(Defaults.itSettings): _*)
+  .settings(
+    Keys.fork in IntegrationTest := false,
+    unmanagedSourceDirectories in IntegrationTest := (baseDirectory in IntegrationTest)(base => Seq(base / "it")).value,
+    testGrouping in IntegrationTest := TestPhases.oneForkedJvmPerTest((definedTests in IntegrationTest).value),
+    parallelExecution in IntegrationTest := false,
+    addTestReportOption(IntegrationTest, "int-test-reports")
+  )
+  .settings(resolvers += Resolver.jcenterRepo)
+  .settings(scoverageSettings)
+
+lazy val scoverageSettings: Seq[Setting[_]] = Seq(
+  coverageExcludedPackages := List(
+    "<empty>"
+    ,"Reverse.*"
+    ,"domain\\..*"
+    ,"models\\..*"
+    ,"metrics\\..*"
+    ,".*(BuildInfo|Routes|Options).*"
+  ).mkString(";"),
+  coverageMinimum := 95,
+  coverageFailOnMinimum := true,
+  coverageHighlighting := true,
+  parallelExecution in Test := false
+)
