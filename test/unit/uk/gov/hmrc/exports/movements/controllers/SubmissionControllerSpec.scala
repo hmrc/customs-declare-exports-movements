99ec6eef2d5d5b65e0b7159b6d274ff27128db5d
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-08-21 15:47:34 +0100

    CEDS-1509 Remove unused fields in SubmissionControllerSpec

--- test/unit/uk/gov/hmrc/exports/movements/controllers/SubmissionControllerSpec.scala
+++ test/unit/uk/gov/hmrc/exports/movements/controllers/SubmissionControllerSpec.scala
@@ -31,8 +31,6 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.auth.core.AuthConnector
 import uk.gov.hmrc.exports.movements.controllers.util.CustomsHeaderNames.XEoriIdentifierHeaderName
-import uk.gov.hmrc.exports.movements.controllers.util.HeaderValidator
-import uk.gov.hmrc.exports.movements.metrics.MovementsMetrics
 import uk.gov.hmrc.exports.movements.models.submissions.ActionType
 import uk.gov.hmrc.exports.movements.services.SubmissionService
 import uk.gov.hmrc.exports.movements.services.context.SubmissionRequestContext
@@ -58,8 +56,6 @@ class SubmissionControllerSpec
   private def getSubmissionUri(conversationId: String) = s"/movements/$conversationId"
 
   private val submissionServiceMock = buildSubmissionServiceMock
-  implicit private val metrics: MovementsMetrics = app.injector.instanceOf[MovementsMetrics]
-  implicit private val headerValidator: HeaderValidator = app.injector.instanceOf[HeaderValidator]
 
   override def beforeEach(): Unit = {
     super.beforeEach()
