6346e3a237233a34da33c965704fcdbd554ca025
Tim <glidester@gmail.com>
2023-11-23 16:22:08 +0000

    CEDS-5275 Add support for Anticipated Arrivals EAA messages

--- test/unit/uk/gov/hmrc/exports/movements/models/notifications/parsers/ResponseParserProviderSpec.scala
+++ test/unit/uk/gov/hmrc/exports/movements/models/notifications/parsers/ResponseParserProviderSpec.scala
@@ -73,6 +73,9 @@ class ResponseParserProviderSpec extends AnyWordSpec with Matchers {
         val parser = parserFactory.provideResponseParser(responseXml)
 
         parser mustBe a[IleQueryResponseParser]
+
+        val out = parser.parse(responseXml)
+        println(out)
       }
     }
 
